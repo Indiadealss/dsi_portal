798c867432c86ee291182332caf0feb5a0c8325e
Indiadealss <indiadealss1@gmail.com>
2026-03-09 13:08:46 +0530

    add all files

--- src/component/Seo.jsx
+++ src/component/Seo.jsx
@@ -4,14 +4,21 @@ import { useParams } from 'react-router-dom';
 
 const Seo = ({title, description,canonical}) => {
   const { slug } = useParams();
+
+  console.log(title,'title',description,'desctiption',canonical,'canoniacal','9897');
+  
   
 
   return (
     <Helmet>
       <title>{title}</title>
-      {description && <meta name='description' content={description} />}
+      {description && (
+        <meta name="description" content={description} />
+      )}
 
-        {canonical && <link rel="canonical" href={canonical} />}
+      {canonical && (
+        <link rel="canonical" href={canonical} />
+      )}
     </Helmet>
   )
 }
